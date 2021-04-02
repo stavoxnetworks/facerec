7517bee824d0598eca76738b2da82de7f221af20
sweaver-rvcs <57680197+sweaver-rvcs@users.noreply.github.com>
2021-04-02 12:14:15 -0400

    first commit

---
+++ FaceRec/src/main/java/business_logic/MySQL.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business_logic;
+
+/**
+ *
+ * @author Jinkal
+ */
+public class MySQL 
+{
+        public String HOST = "jdbc:mysql://localhost:3306/project";
+        public String USERNAME = "root";
+        public String PASSWORD = "1";
+        public MySQL()
+        {
+            
+        }
+}
